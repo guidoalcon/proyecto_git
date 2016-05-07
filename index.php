ccda44f871ee242b683543e762ed5918c3f1d844
guidex <guedux@gmail.com>
2016-05-07 11:45:02 -0400

    Se agrego parrafos

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 echo 'hola mundo desde PHP';
 
-
 ?>
 <html>
 	<head></head>
@@ -11,5 +10,21 @@ echo 'hola mundo desde PHP';
 	<h1>BIENVENIDOS A NUESTRA PRIMERA PAGINA WEB </h1>
 
 	</body>
+	<p>
+		
+	Hola este es un proyecto prieba para el uso de Git
+
+	</p>
+
+	<p>
+		
+		Nuestro objetivo es tan claro , llegar a cumplir terminar la tesis
+	</p>
+
+	<p>
+		
+	Vamos  siempre encaminado con el amro de Dios.
+
+	</p>
 
 </html>
